359d95d5600d7104c4129cac94581f0ff149d91d
Travis Weir <tweirtx@gmail.com>
2018-05-22 18:12:34 -0500

    Made a few more changes

--- getAPIKey.py
+++
@@ -1,2 +0,0 @@
-class key():
-	tba = '5nxF8pYtXPy1rZELO68JhjwYZuJRkojuPNDUuEAXyysRn8A6SfL1GaAvbYskU6rj'
--- recorder.py
+++ recorder.py
@@ -6,70 +6,76 @@ tba = tbapi.TBAParser(api_key=getAPIKey.key.tba, cache=False)
 
 
 class streamdb(db.DatabaseObject):
-	__tablename__ = 'streamstarts'
-	eventkey = db.Column(db.String)
-	starttime = db.Column(db.String)
-	primary_key = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'streamstarts'
+    eventkey = db.Column(db.String)
+    starttime = db.Column(db.String)
+    primary_key = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
 
 db.DatabaseObject.metadata.create_all()
 debug = True
 if debug:
-	eventkey = '2018txlu'
+    eventkey = '2018vapor'
 else:
-	eventkey = input("Please enter the event key as it appears in TBA: ")
+    eventkey = input("Please enter the event key as it appears in TBA: ")
 try:
-	eventtba = tba.get_event(event_key=eventkey)
+    eventtba = tba.get_event(event_key=eventkey)
 except tbapi.InvalidKeyError:
-	raise ValueError('That event does not exist!')
+    raise ValueError('That event does not exist!')
 webcast = eventtba.webcasts[0]
 if debug:
-	print(webcast)
+    print(webcast.channel)
 
 stream = streamlink.Streamlink()
 
 
 def logstart():
-	with db.Session() as session:
-		starting = streamdb(eventkey=eventkey, starttime='{}:{}:{}'.format(datetime.datetime.now().hour,
-																		datetime.datetime.now().minute,
-																		   datetime.datetime.now().second))
-		session.add(starting)
+    with db.Session() as session:
+        starting = streamdb(eventkey=eventkey, starttime='{}:{}:{}'.format(datetime.datetime.now().hour,
+                                                                        datetime.datetime.now().minute,
+                                                                           datetime.datetime.now().second))
+        session.add(starting)
 
 
 def checkiflive():
-	streams = stream.streams(url)
-	try:
-		streams['best']
-		return True
-	except KeyError:
-		return False
+    streams = stream.streams(url)
+    try:
+        streams['best']
+        return True
+    except KeyError:
+        return False
 
 
 def waituntillive():
-	while True:
-		is_live = checkiflive()
-		if is_live:
-			break
-		print("Stream is not live yet, waiting 30 seconds and retrying")
-		sleep(30)
+    while True:
+        is_live = checkiflive()
+        if is_live:
+            break
+        print("Stream is not live yet, waiting 30 seconds and retrying")
+        sleep(30)
 
 
 if webcast.type == 'twitch':
-	url = 'https://twitch.tv/{}'.format(webcast.channel)
+    url = 'https://twitch.tv/{}'.format(webcast.channel)
 elif webcast.type == 'dacast':
-	print("Sorry, we can't work with this streaming provider due to required info being behind a paywall. Please bug FiM to stop using DaCast.")
-	exit(0)
+    print("Sorry, we can't work with this streaming provider due to required info being behind a paywall. Please bug FiM to stop using DaCast.")
+    exit(0)
+elif webcast.type == 'livestream':
+    url = 'https://livestream.com/{}'.format(webcast.channel)
 else:
-	print("This event's stream is currently unsupported")
-	exit()
+    print("This event's stream is currently unsupported")
+    exit()
 if debug:
-	url = 'https://twitch.tv/tweirtx' # Debug only, remove before production use!
+    url = 'https://livestream.com/accounts/27110937/' # Debug only, remove before production use!
 
 waituntillive()
 print("Now live!")
+logstart()
 streams = stream.streams(url)
 stream_file = streams['best'].open()
 with open('{}.mp4'.format(eventkey), 'wb') as f:
-	while checkiflive():
-		f.write(stream_file.read(1024))
+    while True:
+        stream_data = stream_file.read(10240)
+        if stream_data == b'':
+            break
+        f.write(stream_data)
