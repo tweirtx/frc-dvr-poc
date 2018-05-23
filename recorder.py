743ea0ef39a4016fba613f30f8d258996dcd2e82
Travis Weir <tweirtx@gmail.com>
2018-05-23 12:32:56 -0500

    Made more things variable, added .idea to gitignore

--- recorder.py
+++ recorder.py
@@ -9,6 +9,7 @@ class streamdb(db.DatabaseObject):
     __tablename__ = 'streamstarts'
     eventkey = db.Column(db.String)
     starttime = db.Column(db.String)
+    day = db.Column(db.String)
     primary_key = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
 
--- splitter.py
+++ splitter.py
@@ -1,9 +1,15 @@
-import tbapi, datetime, tbapy
+import datetime, tbapy
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import getAPIKey, db
+from recorder import streamdb
 
-tba = tbapi.TBAParser(api_key='5nxF8pYtXPy1rZELO68JhjwYZuJRkojuPNDUuEAXyysRn8A6SfL1GaAvbYskU6rj', cache=False)
+tbap = tbapy.TBA(getAPIKey.Key.tba)
 
-tbap = tbapy.TBA('5nxF8pYtXPy1rZELO68JhjwYZuJRkojuPNDUuEAXyysRn8A6SfL1GaAvbYskU6rj')
+
+def get_start_time(event, day):
+    with db.Session as session:
+        session.query(streamdb).filter_by(eventkey=event, day=day)
+    return "Todo"
 
 
 def splitter(day, streamtime, i):
@@ -13,23 +19,17 @@ def splitter(day, streamtime, i):
     ffmpeg_extract_subclip('{}.mp4'.format(day), starttime, endtime, '{}.mp4'.format(i.key))
 
 
-matches = tbap.event_matches('2018txsa')
-for i in matches:
-    print(i.actual_time < 1523077200)
-    try:
-        open('{}.mp4'.format(i.key))
-        print('{} already exists'.format(i.key))
-        already_exists = True
-    except FileNotFoundError:
-        print("{} does not already exist".format(i.key))
-        already_exists = False
-    if i.actual_time < 1523077200:
-        day = 'friday'
-        time = tw.videos.get_by_id(247255213)['created_at'].timestamp()
+def split(event):
+    matches = tbap.event_matches(event)
+    for i in matches:
+        try:
+            open('{}.mp4'.format(i.key))
+            print('{} already exists'.format(i.key))
+            already_exists = True
+        except FileNotFoundError:
+            print("{} does not already exist".format(i.key))
+            already_exists = False
+        day = datetime.datetime.fromtimestamp(i.actual_time).weekday()
+        time = get_start_time(event, day)
         if i.actual_time and not already_exists:
-            splitter(day, time, i)
-    else:
-        day = 'saturday'
-        time = tw.videos.get_by_id(247649045)['created_at'].timestamp()
-        if i.actual_time and not already_exists:
-            splitter(day, time, i)
+                splitter(day, time, i)
