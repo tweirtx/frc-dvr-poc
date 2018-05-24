0dcce72e380d7ecb9a7b01feecaed3b8ec212158
Travis Weir <tweirtx@gmail.com>
2018-05-24 14:11:25 -0500

    Added day back

--- streamdb.py
+++ streamdb.py
@@ -4,7 +4,8 @@ import db
 class streamdb(db.DatabaseObject):
     __tablename__ = 'streamstarts'
     eventkey = db.Column(db.String)
-    starttime = db.Column(db.String)
+    starttime = db.Column(db.Integer)
+    day = db.Column(db.String)
     primary_key = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
 
